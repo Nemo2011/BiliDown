ea7c6b305c7511d7dfed6c8eeadea78b82be7e1b
Nemo2011 <yimoxia@outlook.com>
2023-03-09 21:52:15 +0800

    1.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf8") as f:
 
 setuptools.setup(
     name="BiliDown",
-    version="1.2.1",
+    version="1.2.2",
     license="GPLv3+",
     author="Nemo2011",
     description="哔哩哔哩命令行下载器",
